32e92629f78dd656da93c5d6f87c16057610d3c2
Anirudh mamilla <104907727+Perkywarcheif@users.noreply.github.com>
2025-01-03 19:16:22 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -130,5 +130,5 @@ def main():
 
         st.write("In this project, a chatbot is built that can understand and respond to user input based on intents. The chatbot was trained using NLP and Logistic Regression, and the interface was built using Streamlit. This project can be extended by adding more data, using more sophisticated NLP techniques, deep learning algorithms.")
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
