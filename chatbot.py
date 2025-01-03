b75ea830460d7dd852613d90c1702996c40d5715
Anirudh mamilla <104907727+Perkywarcheif@users.noreply.github.com>
2025-01-03 19:06:51 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -54,5 +54,5 @@ def main():
     elif choice == "About":
         st.write("The goal of this project is to create a chatbot that can understand and respond to user input based on intents. The chatbot is built using Natural Language Processing (NLP) library and Logistic Regression, to extract the intents and entities from user input. The chatbot is built using Streamlit, a Python library for building interactive web applications.")
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
